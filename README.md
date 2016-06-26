f095a03fca52d7d856f01940e5531ef17aa7264f
Andrus Adamchik <andrus@objectstyle.com>
2016-06-26 17:53:02 -0400

    docs

--- README.md
+++ README.md
@@ -4,5 +4,43 @@
 
 Integration of Jooq SQL builder with Bootique.
 
+## Usage
+
+Include ```bootique-jooq```:
+```xml
+<dependency>
+	<groupId>io.bootique.jooq</groupId>
+	<artifactId>bootique-jooq</artifactId>
+</dependency>
+```
+
+Do whatever Jooq class generation is required (for now outside of bootique-jooq scope).
+
+Create configuration with DB connection information and optional Jooq settings:
+```yaml
+jdbc:
+  default:
+      url: "jdbc:postgresql://192.168.99.100:5432/scheduling"
+      initialSize: 1
+      username: postgres
+      password: postgres
+
+jooq:
+  dialect: POSTGRES
+  executeLogging: true
+```
+Inject ```JooqFactory``` and start using Jooq:
+```java
+@Inject
+private JooqFactory jooqFactory;
+
+public void doSomething() {
+	try (DSLContext c = jooqFactory.newContext()) {
+	    Record r = c.select().from(Tables.MY_TABLE).fetchOne();
+	}
+}
+```
+
+
 
 
--- src/main/java/io/bootique/jooq/JooqFactory.java
+++ src/main/java/io/bootique/jooq/JooqFactory.java
@@ -8,18 +8,18 @@ import org.jooq.DSLContext;
 public interface JooqFactory {
 
     /**
-     * Creates and returns a new context for the "default" DataSource. Only works if there one DataSource available
-     * in your Bootoque app. It is the caller's responsibility to close the context.
+     * Creates and returns a new context for the "default" DataSource. Only works if there one and only one
+     * DataSource available in your Bootoque app. It is caller's responsibility to close the returned context.
      *
      * @return a new {@link DSLContext}.
      */
     DSLContext newContext();
 
     /**
-     * Creates and returns a new context for the named DataSource. It is the caller's responsibility to
-     * close the context.
+     * Creates and returns a new context for the named DataSource. It is caller's responsibility to close the
+     * returned context.
      *
-     * @param dataSource DataSource name mapped in Bootique.
+     * @param dataSource the name of a DataSource mapped in Bootique.
      * @return a new {@link DSLContext}.
      */
     DSLContext newContext(String dataSource);
